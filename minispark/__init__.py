fe35cc2b55a82b2fdac627ec9a0e7e7ae1a71782
Aditi Ramakrishnan <aditirkrishna@gmail.com>
2026-08-13 21:03:05 +0530

    feat: add package init and version marker

---
+++ minispark/__init__.py
@@ -0,0 +1,6 @@
+"""MiniSpark: a first-principles, single-machine distributed data-processing engine.
+
+Educational/research project. Not a replacement for Apache Spark.
+"""
+
+__version__ = "0.1.0"
